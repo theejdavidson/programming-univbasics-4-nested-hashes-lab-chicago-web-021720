1b7ca4fd2865b94fb20d06ecbeaa688d60555a9d
Ethan Davidson <theejdavidson@gmail.com>
2020-01-25 01:37:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,8 +82,6 @@ def adding_matz
         }
      }
 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"] }}
-return programmer_hash
-    
 end
 
 def changing_alan
